a8d572fde2e11c0e47c90b15e82532b6d109d071
Raphael Riebl <raphael@riebl.net>
2013-06-17 15:07:40 +0200

    add Wgs84Point structure

---
+++ sumo2nmea/wgs84point.h
@@ -0,0 +1,15 @@
+#ifndef WGS84POINT_H_GAHDJKCG
+#define WGS84POINT_H_GAHDJKCG
+
+#include "angle.h"
+
+struct Wgs84Point
+{
+    typedef AngleDegree angle_type;
+    Wgs84Point(angle_type latitude, angle_type longitude) : lat(latitude), lon(longitude) {}
+    angle_type lat;
+    angle_type lon;
+};
+
+#endif /* WGS84POINT_H_GAHDJKCG */
+
