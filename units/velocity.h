70fc64bd2912d05a8153dd96615b85385cc31ce6
Raphael Riebl <raphael@riebl.net>
2013-06-21 15:59:24 +0200

    units: add preliminary support for velocity conversions

--- units/velocity.h
+++ units/velocity.h
@@ -2,20 +2,46 @@
 #define VELOCITY_H_0BLFAIDT
 
 #include "vanetza/units/quantity.h"
+#include "vanetza/units/unit.h"
 
 namespace units
 {
     struct knots {};
+    struct meter_per_second {};
     static const knots kn;
+    static const meter_per_second mps;
 } // namespace units
 
+namespace constants
+{
+    static const unsigned cNauticalMileInMeters = 1852;
+} // namespace constants
+
+template<typename T>
+struct conversion_helper<units::meter_per_second, units::knots, T>
+{
+    static T convert(const T& mps)
+    {
+        return (mps * 3600) / constants::cNauticalMileInMeters;
+    }
+};
+
+template<typename UNIT>
 class Velocity : public Quantity<double>
 {
     public:
     using Quantity<double>::Quantity;
+
+    // TODO: implement generic conversion
+    template<typename OTHER_UNIT>
+    Velocity(const Velocity<OTHER_UNIT>& other) :
+        Quantity<double>(conversion_helper<OTHER_UNIT, UNIT, double>::convert(other.value()))
+    {
+    }
 };
 
-typedef Velocity VelocityKnot;
+typedef Velocity<units::knots> VelocityKnot;
+typedef Velocity<units::meter_per_second> VelocityMps;
 
 #endif /* VELOCITY_H_0BLFAIDT */
 
