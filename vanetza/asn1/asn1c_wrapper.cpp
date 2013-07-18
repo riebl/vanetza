50fb5747a948d17b44f673201225fd177c7b2731
Raphael Riebl <raphael.riebl.inf@haw-ingolstadt.de>
2013-07-18 16:39:47 +0200

    asn1: wrapper class for structs generated by asn1c

---
+++ vanetza/asn1/asn1c_wrapper.cpp
@@ -0,0 +1,105 @@
+#include <vanetza/asn1/gen/asn_application.h>
+#include <vanetza/asn1/gen/constraints.h>
+#include "asn1c_wrapper.hpp"
+#include <vanetza/common/byte_buffer.hpp>
+#include <algorithm>
+#include <cassert>
+#include <memory>
+#include <stdexcept>
+#include <string>
+
+namespace vanetza
+{
+namespace asn1
+{
+
+static int write_buffer(const void* in, std::size_t size, void* out_void)
+{
+    assert(out_void != nullptr);
+    auto out = static_cast<ByteBuffer*>(out_void);
+    std::copy_n(static_cast<const uint8_t*>(in), size, std::back_inserter(*out));
+    return 0;
+}
+
+static int write_null(const void*, std::size_t, void*)
+{
+    return 0;
+}
+
+void* allocate(std::size_t length)
+{
+    void* ptr = calloc(1, length);
+    if (nullptr == ptr) {
+        throw std::runtime_error("Bad ASN.1 memory allocation");
+    }
+    return ptr;
+}
+
+void free(asn_TYPE_descriptor_t& td, void* t)
+{
+    ASN_STRUCT_FREE(td, t);
+}
+
+void* copy(asn_TYPE_descriptor_t& td, const void* original)
+{
+    void* copy = nullptr;
+    ByteBuffer buffer;
+
+    asn_enc_rval_t ec;
+    ec = der_encode(&td, const_cast<void*>(original), write_buffer, &buffer);
+    if (ec.encoded == -1) {
+        throw std::runtime_error("DER encoding failed");
+    }
+
+    asn_dec_rval_t dc;
+    dc = ber_decode(0, &td, &copy, &buffer[0], buffer.size());
+    if (dc.code != RC_OK) {
+        free(td, copy);
+        throw std::runtime_error("BER decoding failed");
+    }
+
+    return copy;
+}
+
+bool validate(asn_TYPE_descriptor_t& td, const void* t)
+{
+    return asn_check_constraints(&td, t, nullptr, nullptr) == 0;
+}
+
+bool validate(asn_TYPE_descriptor_t& td, const void* t, std::string& error)
+{
+    char errbuf[1024];
+    std::size_t errlen = sizeof(errbuf);
+    bool ok = asn_check_constraints(&td, t, errbuf, &errlen) == 0;
+    if (!ok) {
+        error = errbuf;
+    }
+    return ok;
+}
+
+std::size_t size(asn_TYPE_descriptor_t& td, const void* t)
+{
+    asn_enc_rval_t ec;
+    ec = uper_encode(&td, const_cast<void*>(t), write_null, 0);
+    if (ec.encoded < 0) {
+        throw std::runtime_error("Can't determine size for unaligned PER encoding");
+    }
+
+    // Caution! ec.encoded are bits not bytes!
+    return (ec.encoded + 7) / 8;
+}
+
+ByteBuffer encode(asn_TYPE_descriptor_t& td, const void* t)
+{
+    ByteBuffer buffer;
+    asn_enc_rval_t ec;
+    ec = uper_encode(&td, const_cast<void*>(t), write_buffer, &buffer);
+    if (ec.encoded == -1) {
+        throw std::runtime_error("Unaligned PER encoding failed");
+    }
+    return buffer;
+}
+
+} // namespace asn1
+} // namespace vanetza
+
---
+++ vanetza/asn1/asn1c_wrapper.hpp
@@ -0,0 +1,106 @@
+#ifndef ASN1C_WRAPPER_HPP_ZCNDO8E5
+#define ASN1C_WRAPPER_HPP_ZCNDO8E5
+
+#include <vanetza/asn1/gen/constr_TYPE.h>
+#include <vanetza/common/byte_buffer.hpp>
+#include <cstddef>
+#include <string>
+
+namespace vanetza
+{
+namespace asn1
+{
+
+void* allocate(std::size_t);
+void free(asn_TYPE_descriptor_t&, void*);
+void* copy(asn_TYPE_descriptor_t&, const void*);
+bool validate(asn_TYPE_descriptor_t&, const void*);
+bool validate(asn_TYPE_descriptor_t&, const void*, std::string&);
+std::size_t size(asn_TYPE_descriptor_t&, const void*);
+ByteBuffer encode(asn_TYPE_descriptor_t&, const void*);
+
+template<class T>
+T* allocate()
+{
+    return static_cast<T*>(allocate(sizeof(T)));
+}
+
+template<class T>
+class asn1c_wrapper
+{
+public:
+    typedef T asn1c_type;
+
+    asn1c_wrapper(asn_TYPE_descriptor_t& desc) :
+        m_struct(vanetza::asn1::allocate<asn1c_type>()), m_type(desc) {}
+    ~asn1c_wrapper() { vanetza::asn1::free(m_type, m_struct); }
+
+    // copy semantics
+    asn1c_wrapper(const asn1c_wrapper& other) :
+        m_struct(nullptr), m_type(other.m_type)
+    {
+        void* copy = vanetza::asn1::copy(m_type, other.m_struct);
+        m_struct = static_cast<asn1c_type*>(copy);
+    }
+
+    asn1c_wrapper& operator=(const asn1c_wrapper& other)
+    {
+        using namespace vanetza::asn1;
+        free(m_type, m_struct);
+        m_struct = copy(other.m_struct);
+        m_type = other.m_type;
+        return *this;
+    }
+
+    // dereferencing
+    asn1c_type& operator*() { return *m_struct; }
+    asn1c_type* operator->() { return m_struct; }
+
+    /**
+     * Check ASN.1 constraints
+     * \param error (optional) copy of error message
+     * \return true if valid
+     */
+    bool validate() const
+    {
+        return vanetza::asn1::validate(m_type, m_struct);
+    }
+
+    /**
+     * Check ASN.1 constraints
+     * \param error Error message if any constraint failed
+     * \return true if valid
+     */
+    bool validate(std::string& error) const
+    {
+        return vanetza::asn1::validate(m_type, m_struct, error);
+    }
+
+    /**
+     * Encode ASN.1 struct into byte buffer
+     * \return byte buffer containing serialized ASN.1 struct
+     */
+    ByteBuffer encode() const
+    {
+        return vanetza::asn1::encode(m_type, m_struct);
+    }
+
+    /**
+     * Get size of encoded ASN.1 struct
+     * \return size in bytes
+     */
+    std::size_t size() const
+    {
+        return vanetza::asn1::size(m_type, m_struct);
+    }
+
+private:
+    asn1c_type* m_struct;
+    asn_TYPE_descriptor_t& m_type;
+};
+
+} // namespace asn1
+} // namespace vanetza
+
+#endif /* ASN1C_WRAPPER_HPP_ZCNDO8E5 */
+
