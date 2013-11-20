1fae0dc285e4c3c09c1bd380941381a1a9e4c5e1
Raphael Riebl <raphael.riebl@thi.de>
2013-11-20 18:37:32 +0100

    geonet: add ExtendedPduRefs, variant of ExtendedPdu

--- vanetza/geonet/extended_pdu.hpp
+++ vanetza/geonet/extended_pdu.hpp
@@ -19,6 +19,13 @@ template<class HEADER>
 void serialize(const ExtendedPdu<HEADER>&, OutputArchive&);
 
 template<class HEADER>
+class ExtendedPduRefs;
+
+template<class HEADER>
+void serialize(const ExtendedPduRefs<HEADER>&, OutputArchive&);
+
+
+template<class HEADER>
 class ExtendedPdu : public Pdu
 {
 public:
@@ -56,7 +63,51 @@ private:
 };
 
 template<class HEADER>
+class ExtendedPduRefs : public Pdu
+{
+public:
+    ExtendedPduRefs(BasicHeader& basic, CommonHeader& common, HEADER& extended) :
+        mr_basic(basic), mr_common(common), mr_extended(extended) {}
+    BasicHeader& basic() { return mr_basic; }
+    const BasicHeader& basic() const { return mr_basic; }
+    CommonHeader& common() { return mr_common; }
+    const CommonHeader& common() const { return mr_common; }
+    HEADER& extended() { return mr_extended; }
+    const HEADER& extended() const { return mr_extended; }
+
+    ExtendedPdu<HEADER>* clone() const
+    {
+        return new ExtendedPdu<HEADER>(mr_basic, mr_common, mr_extended);
+    }
+
+    std::size_t length() const
+    {
+        return BasicHeader::length_bytes +
+            CommonHeader::length_bytes +
+            HEADER::length_bytes;
+    }
+
+    void serialize(OutputArchive& ar) const
+    {
+        geonet::serialize(*this, ar);
+    }
+
+private:
+    BasicHeader& mr_basic;
+    CommonHeader& mr_common;
+    HEADER& mr_extended;
+};
+
+template<class HEADER>
 void serialize(const ExtendedPdu<HEADER>& pdu, OutputArchive& ar)
+{
+    serialize(pdu.basic(), ar);
+    serialize(pdu.common(), ar);
+    serialize(pdu.extended(), ar);
+}
+
+template<class HEADER>
+void serialize(const ExtendedPduRefs<HEADER>& pdu, OutputArchive& ar)
 {
     serialize(pdu.basic(), ar);
     serialize(pdu.common(), ar);
