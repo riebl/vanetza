6619e64fcaed5c3e416a55f75318df277fedeeb0
Raphael Riebl <raphael.riebl@thi.de>
2013-11-25 20:40:00 +0100

    geonet: add PDU interface

---
+++ vanetza/geonet/pdu.hpp
@@ -0,0 +1,32 @@
+#ifndef PDU_HPP_PQEC9PDO
+#define PDU_HPP_PQEC9PDO
+
+#include <vanetza/geonet/serialization.hpp>
+#include <cstddef>
+
+namespace vanetza
+{
+namespace geonet
+{
+
+struct BasicHeader;
+struct CommonHeader;
+
+class Pdu
+{
+public:
+    virtual BasicHeader& basic() = 0;
+    virtual const BasicHeader& basic() const = 0;
+    virtual CommonHeader& common() = 0;
+    virtual const CommonHeader& common() const = 0;
+    virtual void serialize(OutputArchive&) const = 0;
+    virtual Pdu* clone() const = 0;
+    virtual std::size_t length() const = 0;
+    virtual ~Pdu() {}
+};
+
+} // namespace geonet
+} // namespace vanetza
+
+#endif /* PDU_HPP_PQEC9PDO */
+
