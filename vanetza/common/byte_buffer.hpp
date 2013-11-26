eb13eec7ad013db6e51c7bd8696a3450dfa48f67
Raphael Riebl <raphael.riebl@thi.de>
2013-11-26 20:44:35 +0100

    common: ByteBuffer: buffer_cast function

--- vanetza/common/byte_buffer.hpp
+++ vanetza/common/byte_buffer.hpp
@@ -2,12 +2,38 @@
 #define BYTE_BUFFER_HPP_7NOEQO4F
 
 #include <cstdint>
+#include <type_traits>
 #include <vector>
 
 namespace vanetza
 {
 
 typedef std::vector<uint8_t> ByteBuffer;
+
+/**
+ * Cast byte buffer to a POD object
+ * \param buffer byte buffer containing requested object
+ * \return pointer to object or nullptr if cast is impossible
+ */
+template<typename MASK>
+MASK* buffer_cast(ByteBuffer& buffer)
+{
+    static_assert(std::is_pod<MASK>::value, "MASK has to be POD data type");
+    static_assert(std::is_object<MASK>::value, "MASK has to be an object");
+
+    MASK* mask = nullptr;
+    if (sizeof(MASK) <= buffer.size()) {
+        mask = reinterpret_cast<MASK*>(&buffer[0]);
+    }
+    return mask;
+}
+
+template<typename MASK>
+const MASK* buffer_cast(const ByteBuffer& buffer)
+{
+    // const_cast is safe, const qualifier is added to return type
+    return buffer_cast<MASK>(const_cast<ByteBuffer&>(buffer));
+}
 
 } // namespace vanetza
 
