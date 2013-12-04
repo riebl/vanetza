0a198c2b0ac77567616173955973c2304506ab87
Raphael Riebl <raphael.riebl@thi.de>
2013-12-04 10:37:30 +0100

    remove units.hpp

--- vanetza/units.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef UNITS_HPP_4VRJDSCB
-#define UNITS_HPP_4VRJDSCB
-
-#include <vanetza/units/angle.hpp>
-#include <vanetza/units/length.hpp>
-#include <vanetza/units/velocity.hpp>
-
-#endif /* UNITS_HPP_4VRJDSCB */
-
