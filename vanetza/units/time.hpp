2f1fa5c1080e220e8b9be7ffbf8c3da21a1dc5fe
Raphael Riebl <raphael.riebl@thi.de>
2013-12-18 09:29:52 +0100

    units: add time related quantity Duration

---
+++ vanetza/units/time.hpp
@@ -0,0 +1,19 @@
+#ifndef TIME_HPP_GFC1AX6E
+#define TIME_HPP_GFC1AX6E
+
+#include <boost/units/quantity.hpp>
+#include <boost/units/systems/si/time.hpp>
+
+namespace vanetza
+{
+namespace units
+{
+
+namespace si = boost::units::si;
+typedef boost::units::quantity<boost::units::si::time> Duration;
+
+} // namespace units
+} // namespace vanetza
+
+#endif /* TIME_HPP_GFC1AX6E */
+
