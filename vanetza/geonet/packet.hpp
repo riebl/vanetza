d3d788d64c25c6bff1fc076ea7a6256ea4065e14
Raphael Riebl <raphael.riebl@thi.de>
2014-01-29 21:09:38 +0100

    geonet: add packet header

---
+++ vanetza/geonet/packet.hpp
@@ -0,0 +1,19 @@
+#ifndef PACKET_HPP_LILZ0UWN
+#define PACKET_HPP_LILZ0UWN
+
+#include <vanetza/net/chunk_packet.hpp>
+#include <vanetza/net/cohesive_packet.hpp>
+
+namespace vanetza
+{
+namespace geonet
+{
+
+typedef ChunkPacket DownPacket;
+typedef CohesivePacket UpPacket;
+
+} // namespace geonet
+} // namespace vanetza
+
+#endif /* PACKET_HPP_LILZ0UWN */
+
