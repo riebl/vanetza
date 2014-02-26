5ed50a636c940182526251d03adfa47f2fa71676
Raphael Riebl <raphael.riebl@thi.de>
2014-02-26 17:02:03 +0100

    geonet: add compile switch for packet variant

--- vanetza/geonet/packet.hpp
+++ vanetza/geonet/packet.hpp
@@ -3,14 +3,26 @@
 
 #include <vanetza/net/chunk_packet.hpp>
 #include <vanetza/net/cohesive_packet.hpp>
+#include <boost/variant.hpp>
 
 namespace vanetza
 {
 namespace geonet
 {
+namespace detail
+{
+
+typedef boost::variant<ChunkPacket, CohesivePacket> PacketVariant;
+
+} // namespace detail
 
 typedef ChunkPacket DownPacket;
+
+#ifdef VANETZA_GEONET_USE_PACKET_VARIANT
+typedef typename detail::PacketVariant UpPacket;
+#else
 typedef CohesivePacket UpPacket;
+#endif
 
 } // namespace geonet
 } // namespace vanetza
