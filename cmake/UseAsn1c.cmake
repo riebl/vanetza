7d95d5a4ae8fe2a720ffa4281cb6b6382e6a7c9e
Raphael Riebl <raphael.riebl@thi.de>
2014-04-11 15:06:01 +0200

    cmake: asn1: fix missing symbols issue in asn1 shared object

--- cmake/UseAsn1c.cmake
+++ cmake/UseAsn1c.cmake
@@ -1,13 +1,8 @@
-macro(add_asn1c DIR TARGET)
+macro(asn1c_sources VAR DIR)
     file(GLOB _sources ${DIR}/*.c)
     list(REMOVE_ITEM _sources ${DIR}/converter-sample.c)
     if(NOT _sources)
         message(FATAL_ERROR "Generate sources from ASN.1 specification in ${DIR} using asn1c at first!")
     endif()
-
-    add_library(${TARGET} STATIC ${_sources})
-    set_target_properties(${TARGET} PROPERTIES
-        INCLUDE_DIRECTORIES ${DIR}
-        INTERFACE_INCLUDE_DIRECTORIES ${DIR}
-        COMPILE_FLAGS -fPIC)
+    set(${VAR} ${_sources})
 endmacro()
