b4a643876039cffd1bdbf1d4692b45037d077395
Raphael Riebl <raphael.riebl@thi.de>
2015-05-26 09:59:13 +0200

    btp: ports: remove superfluous semicolon
    Change-Id: I5bcce043e5f2a66ce9e1b6e707ce71d4f32350a6

--- vanetza/btp/ports.hpp
+++ vanetza/btp/ports.hpp
@@ -14,7 +14,7 @@ namespace ports
 {
 
 static const port_type CAM = host_cast<uint16_t>(2001);
-static const port_type DENM = host_cast<uint16_t>(2002);;
+static const port_type DENM = host_cast<uint16_t>(2002);
 static const port_type TOPO = host_cast<uint16_t>(2003);
 static const port_type SPAT = host_cast<uint16_t>(2004);
 static const port_type SAM = host_cast<uint16_t>(2005);
