49bbb115265237d0078468e6f4a7ddcdde085365
Simon Varga <siv1249@thi.de>
2015-12-18 13:05:42 +0100

    security: add documentation to profile
    Change-Id: I534a1046e5ebdf41aa30be89ad83a85c5584a84d

--- vanetza/security/profile.hpp
+++ vanetza/security/profile.hpp
@@ -8,15 +8,16 @@ namespace vanetza
 namespace security
 {
 
+/** \brief Profile types specified in TS 103 097 v1.2.1 in 7.1, 7.2 and 7.3
+ */
 enum class Profile : uint8_t
 {
     Generic = 0,
     CAM = 1,
-    DENM = 2
+    DENM = 2,
 };
 
 } // namespace security
 } // namespace vanetza
 
 #endif /* PROFILE_HPP_K04O16SX */
-
