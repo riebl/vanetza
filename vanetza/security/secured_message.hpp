f2e827157bc1cc8a15734297070148cd1c86da4a
Simon Varga <siv1249@thi.de>
2016-02-10 13:47:16 +0100

    security: improve documentation of secured_message
    Change-Id: I520bd09dd9959360edd816b052705c7f90feef60

--- vanetza/security/secured_message.hpp
+++ vanetza/security/secured_message.hpp
@@ -1,18 +1,18 @@
 #ifndef SECURED_MESSAGE_HPP_MO3HBSXG
 #define SECURED_MESSAGE_HPP_MO3HBSXG
 
-#include <cstdint>
-#include <list>
 #include <vanetza/security/header_field.hpp>
 #include <vanetza/security/trailer_field.hpp>
 #include <vanetza/security/payload.hpp>
-
+#include <cstdint>
+#include <list>
 
 namespace vanetza
 {
 namespace security
 {
 
+/// SecuredMessage as specified in TS 103 097 v1.2.1, section 5.1
 struct SecuredMessageV2
 {
     std::list<HeaderField> header_fields;
@@ -25,33 +25,32 @@ struct SecuredMessageV2
 using SecuredMessage = SecuredMessageV2;
 
 /**
- * Calculates size of a SecuredMessage object
- * \param SecuredMessage to calculate size
+ * \brief Calculates size of a SecuredMessage object
  * \return size_t containing the number of octets needed to serialize the object
  */
 size_t get_size(const SecuredMessage&);
 
 /**
- * Serializes a SecuredMessage into a binary archive
- * \param SecuredMessage to serialize
- * \param achive to serialize in
+ * \brief Serializes a SecuredMessage into a binary archive
  */
 void serialize(OutputArchive& ar, const SecuredMessage& message);
 
 /**
- * Deserializes a SecuredMessage from a binary archive
- * \param archive with a serialized SecuredMessage at the beginning,
- * \param SecuredMessage to safe deserialized values in
+ * \brief Deserializes a SecuredMessage from a binary archive
  * \return size of deserialized SecuredMessage
  */
 size_t deserialize(InputArchive& ar, SecuredMessage& message);
 
 /**
-* Serialize SecuredMessage and return the ByteBuffer representation,
-* use version, header_fields, payload
-* \param message Message to be converted
-* \return ByteBuffer
-*/
+ * \brief Create ByteBuffer equivalent of SecuredMessage suitable for signature creation
+ *
+ * ByteBuffer contains message's version, header_fields and payload.
+ * Additionally, the length of trailer fields is appended.
+ *
+ * \param message
+ * \param trailer_fields_size Length of trailer fields in bytes
+ * \return serialized data fields relevant for signature creation
+ */
 ByteBuffer convert_for_signing(const SecuredMessage& message, size_t trailer_fields_size);
 
 } // namespace security
--- vanetza/security/tests/check_secured_message.hpp
+++ vanetza/security/tests/check_secured_message.hpp
@@ -14,4 +14,3 @@ void check(const SecuredMessageV2&, const SecuredMessageV2&);
 } // namespace vanetza
 
 #endif /* CHECK_SECURED_MESSAGE_HPP_1YPFNXQA */
-
