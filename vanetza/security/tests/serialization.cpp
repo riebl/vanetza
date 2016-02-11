8ae6db3258c3ca166d94133d9bf5a2180ab09bec
Simon Varga <siv1249@thi.de>
2016-02-11 18:28:20 +0100

    security: simplify buffer_from_hexstring
    Rely on Boost algorithm.
    
    Change-Id: I5ccaa7a98a01d6d4fae01e037396fe47c0a3b48d

--- vanetza/security/tests/serialization.cpp
+++ vanetza/security/tests/serialization.cpp
@@ -1,5 +1,5 @@
 #include <vanetza/security/tests/serialization.hpp>
-#include <cstdio>
+#include <boost/algorithm/hex.hpp>
 
 namespace vanetza
 {
@@ -8,13 +8,8 @@ namespace security
 
 ByteBuffer buffer_from_hexstring(const char* string)
 {
-    unsigned n;
-    const size_t half_string_length = strlen(string) / 2;
-    ByteBuffer buf(half_string_length);
-    for (size_t i = 0; i < half_string_length; ++i) {
-        sscanf(string + 2 * i, "%2X", &n);
-        buf[i] = n;
-    }
+    ByteBuffer buf;
+    boost::algorithm::unhex(string, back_inserter(buf));
     return buf;
 }
 
