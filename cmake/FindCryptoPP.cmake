ee67acef82c5b991e71d0d24e78b91bfd2efa1b9
Raphael Riebl <raphael.riebl@thi.de>
2016-03-01 15:36:07 +0100

    security: fix determination of CryptoPP library version
    Issue found by Markus Monz
    
    Change-Id: Id34ba33b553ef68f62cf20dd8fe0117a88d987b1

--- cmake/FindCryptoPP.cmake
+++ cmake/FindCryptoPP.cmake
@@ -3,8 +3,8 @@ find_library(CryptoPP_LIBRARY NAMES cryptopp DOC "CryptoPP library")
 
 if(CryptoPP_INCLUDE_DIR)
     file(STRINGS ${CryptoPP_INCLUDE_DIR}/cryptopp/config.h _config_version REGEX "CRYPTOPP_VERSION")
-    string(REGEX MATCH "\"([0-9.]+)\"$" _match_version ${_config_version})
-    set(CryptoPP_VERSION ${CMAKE_MATCH_1})
+    string(REGEX MATCH "([0-9])([0-9])([0-9])" _match_version ${_config_version})
+    set(CryptoPP_VERSION "${CMAKE_MATCH_1}.${CMAKE_MATCH_2}.${CMAKE_MATCH_3}")
 endif()
 
 include(FindPackageHandleStandardArgs)
