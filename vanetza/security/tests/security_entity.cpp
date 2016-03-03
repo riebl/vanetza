780f0a40704b8320976f208f8759729e34cedf0b
Raphael Riebl <raphael.riebl@thi.de>
2016-03-03 15:10:28 +0100

    security: import security namespace in SecurityEntity test
    Change-Id: If183a8ecd957ee840cfafa12435f9e4188210bb8

--- vanetza/security/tests/security_entity.cpp
+++ vanetza/security/tests/security_entity.cpp
@@ -5,12 +5,12 @@
 #include <vanetza/security/tests/check_payload.hpp>
 
 using namespace vanetza;
-using vanetza::security::check;
+using namespace vanetza::security;
 
-class SecurityEntity : public ::testing::Test
+class SecurityEntityTest : public ::testing::Test
 {
 protected:
-    SecurityEntity() : sec_ent(time_now)
+    SecurityEntityTest() : sec_ent(time_now)
     {
     }
 
@@ -19,95 +19,95 @@ protected:
         expected_payload[OsiLayer::Transport] = ByteBuffer {89, 27, 1, 4, 18, 85};
     }
 
-    security::EncapRequest create_encap_request()
+    EncapRequest create_encap_request()
     {
-        security::EncapRequest encap_request;
+        EncapRequest encap_request;
         encap_request.plaintext_payload = expected_payload;
-        encap_request.security_profile = security::Profile::CAM;
+        encap_request.security_profile = Profile::CAM;
         return encap_request;
     }
 
     Clock::time_point time_now;
-    security::SecurityEntity sec_ent;
+    SecurityEntity sec_ent;
     ChunkPacket expected_payload;
 };
 
-TEST_F(SecurityEntity, mutual_acceptance)
+TEST_F(SecurityEntityTest, mutual_acceptance)
 {
-    security::SecurityEntity other_sec_ent(time_now);
-    security::EncapConfirm encap_confirm = other_sec_ent.encapsulate_packet(create_encap_request());
-    security::DecapConfirm decap_confirm = sec_ent.decapsulate_packet(security::DecapRequest { encap_confirm.sec_packet });
-    EXPECT_EQ(security::ReportType::Success, decap_confirm.report);
+    SecurityEntity other_sec_ent(time_now);
+    EncapConfirm encap_confirm = other_sec_ent.encapsulate_packet(create_encap_request());
+    DecapConfirm decap_confirm = sec_ent.decapsulate_packet(DecapRequest { encap_confirm.sec_packet });
+    EXPECT_EQ(ReportType::Success, decap_confirm.report);
 }
 
-TEST_F(SecurityEntity, signed_payload_equals_plaintext_payload)
+TEST_F(SecurityEntityTest, signed_payload_equals_plaintext_payload)
 {
-    security::EncapConfirm confirm = sec_ent.encapsulate_packet(create_encap_request());
+    EncapConfirm confirm = sec_ent.encapsulate_packet(create_encap_request());
 
     // check if sec_payload equals plaintext_payload
     check(expected_payload, confirm.sec_packet.payload.data);
 }
 
-TEST_F(SecurityEntity, signature_is_ecdsa)
+TEST_F(SecurityEntityTest, signature_is_ecdsa)
 {
-    security::EncapConfirm confirm = sec_ent.encapsulate_packet(create_encap_request());
+    EncapConfirm confirm = sec_ent.encapsulate_packet(create_encap_request());
 
     // check if trailer_fields contain signature
     ASSERT_EQ(1, confirm.sec_packet.trailer_fields.size());
     // check trailer field type
-    ASSERT_EQ(security::TrailerFieldType::Signature, get_type(confirm.sec_packet.trailer_fields.front()));
+    ASSERT_EQ(TrailerFieldType::Signature, get_type(confirm.sec_packet.trailer_fields.front()));
     // check signature type
-    security::Signature signature = boost::get<security::Signature>(confirm.sec_packet.trailer_fields.front());
-    EXPECT_EQ(security::PublicKeyAlgorithm::Ecdsa_Nistp256_With_Sha256, get_type(signature));
+    Signature signature = boost::get<Signature>(confirm.sec_packet.trailer_fields.front());
+    EXPECT_EQ(PublicKeyAlgorithm::Ecdsa_Nistp256_With_Sha256, get_type(signature));
 }
 
-TEST_F(SecurityEntity, expected_header_field_size)
+TEST_F(SecurityEntityTest, expected_header_field_size)
 {
-    security::EncapConfirm confirm = sec_ent.encapsulate_packet(create_encap_request());
+    EncapConfirm confirm = sec_ent.encapsulate_packet(create_encap_request());
 
     // check header_field size
     EXPECT_EQ(3, confirm.sec_packet.header_fields.size());
 }
 
-TEST_F(SecurityEntity, expected_payload)
+TEST_F(SecurityEntityTest, expected_payload)
 {
-    security::EncapConfirm confirm = sec_ent.encapsulate_packet(create_encap_request());
+    EncapConfirm confirm = sec_ent.encapsulate_packet(create_encap_request());
 
     // check payload
-    security::Payload payload = confirm.sec_packet.payload;
+    Payload payload = confirm.sec_packet.payload;
     EXPECT_EQ(expected_payload.size(), size(payload.data, min_osi_layer(), max_osi_layer()));
-    EXPECT_EQ(security::PayloadType::Signed, get_type(payload));
+    EXPECT_EQ(PayloadType::Signed, get_type(payload));
 }
 
 
-TEST_F(SecurityEntity, test_verify_method)
+TEST_F(SecurityEntityTest, test_verify_method)
 {
     //create signed packet
-    security::EncapRequest encap_request = create_encap_request();
-    security::EncapConfirm encap_confirm;
+    EncapRequest encap_request = create_encap_request();
+    EncapConfirm encap_confirm;
     encap_confirm = sec_ent.encapsulate_packet(encap_request);
 
     //create decap_request
-    security::DecapRequest decap_request(encap_confirm.sec_packet);
+    DecapRequest decap_request(encap_confirm.sec_packet);
 
     //create decap_confirm
-    security::DecapConfirm decap_confirm;
+    DecapConfirm decap_confirm;
     decap_confirm = sec_ent.decapsulate_packet(decap_request);
 
     //check ReportType of decap_confirm
-    EXPECT_EQ(decap_confirm.report, security::ReportType::Success);
+    EXPECT_EQ(decap_confirm.report, ReportType::Success);
 }
 
-TEST_F(SecurityEntity, test_verify_method_fail)
+TEST_F(SecurityEntityTest, test_verify_method_fail)
 {
     //create signed packet
-    security::EncapRequest encap_request = create_encap_request();
-    security::EncapConfirm encap_confirm;
+    EncapRequest encap_request = create_encap_request();
+    EncapConfirm encap_confirm;
     encap_confirm = sec_ent.encapsulate_packet(encap_request);
-    security::SecuredMessage& secured_message = encap_confirm.sec_packet;
+    SecuredMessage& secured_message = encap_confirm.sec_packet;
 
     //create decap_request of signed packet
-    security::DecapRequest decap_request(secured_message);
+    DecapRequest decap_request(secured_message);
 
     //create new (wrong) payload
     ByteBuffer wrong_payload { 7 };
@@ -116,9 +116,9 @@ TEST_F(SecurityEntity, test_verify_method_fail)
     secured_message.payload.data = CohesivePacket(wrong_payload, OsiLayer::Application);
 
     //create decap_confirm
-    security::DecapConfirm decap_confirm;
+    DecapConfirm decap_confirm;
     decap_confirm = sec_ent.decapsulate_packet(decap_request);
 
     //check ReportType of decap_confirm
-    EXPECT_EQ(decap_confirm.report, security::ReportType::False_Signature);
+    EXPECT_EQ(decap_confirm.report, ReportType::False_Signature);
 }
