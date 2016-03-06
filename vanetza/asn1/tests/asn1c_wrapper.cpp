6202d3cfbb3a0e88e8d15806d08af205a824c816
Raphael Riebl <raphael.riebl@thi.de>
2016-03-06 02:54:27 +0100

    asn1: refactor asn1c_wrapper test
    Do not rely on bundled asn1c code which might differ from asn1c invoked by CMake.
    
    Change-Id: I219942d1474189686ca227437b7fa6148e22f6d9

--- vanetza/asn1/tests/asn1c_wrapper.cpp
+++ vanetza/asn1/tests/asn1c_wrapper.cpp
@@ -1,30 +1,35 @@
 #include <gtest/gtest.h>
 #include <vanetza/asn1/asn1c_wrapper.hpp>
-#include "gen/Test.h"
+#include <vanetza/asn1/gen/VanetzaTest.h>
 
 using namespace vanetza::asn1;
-typedef vanetza::asn1::asn1c_wrapper<Test_t> test_wrapper;
+typedef vanetza::asn1::asn1c_wrapper<VanetzaTest_t> test_wrapper;
 
 TEST(asn1c_wrapper, create) {
     EXPECT_NO_THROW({
-        test_wrapper wrapper(asn_DEF_Test);
+        test_wrapper wrapper(asn_DEF_VanetzaTest);
     });
 }
 
+TEST(asn1c_wrapper, constants) {
+    EXPECT_EQ(42, VanetzaTest__field_magicValue);
+}
+
 TEST(asn1c_wrapper, size) {
-    test_wrapper wrapper(asn_DEF_Test);
-    EXPECT_EQ(wrapper.size(), 2);
+    test_wrapper wrapper(asn_DEF_VanetzaTest);
+    OCTET_STRING_fromString(&wrapper->string, "1234");
+    EXPECT_EQ(wrapper.size(), 5);
 }
 
 TEST(asn1c_wrapper, dereferencing) {
-    test_wrapper wrapper(asn_DEF_Test);
+    test_wrapper wrapper(asn_DEF_VanetzaTest);
     ASSERT_NE(wrapper->field, 3);
     wrapper->field = 3;
     EXPECT_EQ((*wrapper).field, 3);
 }
 
 TEST(asn1c_wrapper, copy) {
-    test_wrapper wrapper_orig(asn_DEF_Test);
+    test_wrapper wrapper_orig(asn_DEF_VanetzaTest);
     wrapper_orig->field = 5;
     test_wrapper wrapper_copy = wrapper_orig;
     EXPECT_EQ(wrapper_copy->field, 5);
@@ -33,9 +38,10 @@ TEST(asn1c_wrapper, copy) {
 }
 
 TEST(asn1c_wrapper, validate) {
-    test_wrapper wrapper(asn_DEF_Test);
+    test_wrapper wrapper(asn_DEF_VanetzaTest);
+    OCTET_STRING_fromString(&wrapper->string, "1234");
     EXPECT_TRUE(wrapper.validate());
-    wrapper->field = 832; // out of range
+    OCTET_STRING_fromString(&wrapper->string, "ABCD"); // non-numerics
     EXPECT_FALSE(wrapper.validate());
     std::string msg;
     EXPECT_FALSE(wrapper.validate(msg));
@@ -43,25 +49,27 @@ TEST(asn1c_wrapper, validate) {
 }
 
 TEST(asn1c_wrapper, encode) {
-    test_wrapper wrapper(asn_DEF_Test);
-    wrapper->field = 0xde;
+    test_wrapper wrapper(asn_DEF_VanetzaTest);
+    OCTET_STRING_fromString(&wrapper->string, "1234");
+    wrapper->field = 8;
     vanetza::ByteBuffer buf = wrapper.encode();
     EXPECT_EQ(wrapper.size(), buf.size());
-    // TODO: not completely sure if 0x6f is correct
-    // However, 0x6f contains 0xde bit pattern
-    EXPECT_EQ(vanetza::ByteBuffer({ 0x6f, 0x00 }), buf);
+    // No idea if this is the correct ASN.1 PER equivalent
+    EXPECT_EQ(vanetza::ByteBuffer({ 0x04, 0x02, 0x11, 0xA2, 0x80 }), buf);
 }
 
 TEST(asn1c_wrapper, decode_valid) {
-    test_wrapper wrapper(asn_DEF_Test);
-    const vanetza::ByteBuffer buffer { 0x6f, 0x00 };
+    test_wrapper wrapper(asn_DEF_VanetzaTest);
+    const vanetza::ByteBuffer buffer { 0x04, 0x02, 0x11, 0xA2, 0x80 };
     bool result = wrapper.decode(buffer);
     ASSERT_TRUE(result);
-    EXPECT_EQ(0xde, wrapper->field);
+    EXPECT_EQ(8, wrapper->field);
+    ASSERT_EQ(4, wrapper->string.size);
+    EXPECT_STREQ("1234", (const char*)(wrapper->string.buf));
 }
 
 TEST(asn1c_wrapper, decode_invalid) {
-    test_wrapper wrapper(asn_DEF_Test);
+    test_wrapper wrapper(asn_DEF_VanetzaTest);
     const vanetza::ByteBuffer buffer { 0x12 };
     bool result = wrapper.decode(buffer);
     // should have failed because of short buffer
