6befc7aa07baba4a3801e399c3a09f07edc82b3c
Raphael Riebl <raphael.riebl@thi.de>
2016-09-14 15:47:35 +0200

    gtest: update to release 1.8.0
    Change-Id: I31e110010663962f20b49b511f9ee12e26bdfa1c

--- gtest/GTestFramework.cmake
+++ gtest/GTestFramework.cmake
@@ -1,9 +1,10 @@
-set(GTest_VERSION d945d8c000a0ade73585d143532266968339bbb3)
-set(GTest_ARCHIVE ${GTest_VERSION}.zip)
-set(GTest_ARCHIVE_SHA1 e60441b3b02f0de37a7b1afc3e83ba83aac00f52)
+set(GTest_VERSION 1.8.0)
+set(GTest_ARCHIVE_NAME release-${GTest_VERSION})
+set(GTest_ARCHIVE ${GTest_ARCHIVE_NAME}.tar.gz)
+set(GTest_ARCHIVE_SHA1 e7e646a6204638fe8e87e165292b8dd9cd4c36ed)
 
 set(GTest_DIR ${PROJECT_SOURCE_DIR}/gtest)
-set(GTest_ARCHIVE_DIR ${GTest_DIR}/googletest-${GTest_VERSION}/googletest)
+set(GTest_ARCHIVE_DIR ${GTest_DIR}/googletest-${GTest_ARCHIVE_NAME}/googletest)
 set(GTest_SOURCE_DIR ${GTest_ARCHIVE_DIR}/src)
 set(GTest_INCLUDE_DIR ${GTest_ARCHIVE_DIR}/include)
 
