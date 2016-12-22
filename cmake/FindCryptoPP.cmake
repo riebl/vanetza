07397e302b889c3d55b218b2a9ae98cec3925e81
Raphael Riebl <raphael.riebl@thi.de>
2016-12-22 14:41:46 +0100

    cmake: add imported target for crypto++
    Change-Id: Iab0532045bd1091f8d2f2dccbf1b5fd847918479

--- cmake/FindCryptoPP.cmake
+++ cmake/FindCryptoPP.cmake
@@ -11,3 +11,12 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(CryptoPP
     REQUIRED_VARS CryptoPP_INCLUDE_DIR CryptoPP_LIBRARY
     VERSION_VAR CryptoPP_VERSION)
+
+if(CryptoPP_FOUND AND NOT TARGET CryptoPP::CryptoPP)
+    add_library(CryptoPP::CryptoPP UNKNOWN IMPORTED)
+    set_target_properties(CryptoPP::CryptoPP PROPERTIES
+        IMPORTED_LOCATION "${CryptoPP_LIBRARY}"
+        INTERFACE_INCLUDE_DIRECTORIES "${CryptoPP_INCLUDE_DIR}")
+endif()
+
+mark_as_advanced(CryptoPP_INCLUDE_DIR CryptoPP_LIBRARY)
