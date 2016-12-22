01254553e054f9c665ac334f2fc52fd0fed274aa
Raphael Riebl <raphael.riebl@thi.de>
2016-12-22 14:37:44 +0100

    cmake: add imported target for gpsd
    Change-Id: I7f3e4c4949ef447d1a2a2504f2e39b787a73e3fb

--- cmake/FindGPS.cmake
+++ cmake/FindGPS.cmake
@@ -1,9 +1,19 @@
 find_path(GPS_INCLUDE_DIR NAMES gps.h DOC "libgps include directory")
 find_library(GPS_LIBRARY NAMES gps DOC "libgps library")
 
+if(GPS_INCLUDE_DIR)
+    file(STRINGS ${GPS_INCLUDE_DIR}/gps.h _version_lines REGEX "GPSD_API_(MAJOR|MINOR)_VERSION")
+    string(REGEX MATCH "MAJOR_VERSION[ \t]+([0-9]+)" _version_major ${_version_lines})
+    set(GPS_VERSION_MAJOR ${CMAKE_MATCH_1})
+    string(REGEX MATCH "MINOR_VERSION[ \t]+([0-9]+)" _version_minor ${_version_lines})
+    set(GPS_VERSION_MINOR ${CMAKE_MATCH_1})
+    set(GPS_VERSION "${GPS_VERSION_MAJOR}.${GPS_VERSION_MINOR}")
+endif()
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(GPS
-    REQUIRED_VARS GPS_INCLUDE_DIR GPS_LIBRARY)
+    REQUIRED_VARS GPS_INCLUDE_DIR GPS_LIBRARY
+    VERSION_VAR GPS_VERSION)
 
 if(GPS_FOUND AND NOT TARGET GPS::GPS)
     add_library(GPS::GPS UNKNOWN IMPORTED)
