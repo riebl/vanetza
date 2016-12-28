87c7e55985f71d86ea3ede173648de21d64516dc
Raphael Riebl <raphael.riebl@thi.de>
2016-12-28 19:08:50 +0100

    cmake: FindGeographicLib: harmonise variable names
    CMake manual recommends slightly different variable
    names for find modules
    
    Change-Id: I0f18f45a10d2e11b4ba2b97f0d57c2dab0a9d414

--- cmake/FindGeographicLib.cmake
+++ cmake/FindGeographicLib.cmake
@@ -4,19 +4,22 @@ find_library(GeographicLib_LIBRARY NAMES Geographic DOC "GeographicLib library")
 if(GeographicLib_INCLUDE_DIR)
     file(STRINGS ${GeographicLib_INCLUDE_DIR}/GeographicLib/Config.h _config_version REGEX "GEOGRAPHICLIB_VERSION_STRING")
     string(REGEX MATCH "\"([0-9.]+)\"$" _match_version ${_config_version})
-    set(GeographicLib_VERSION ${CMAKE_MATCH_1})
+    set(GeographicLib_VERSION_STRING ${CMAKE_MATCH_1})
 endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(GeographicLib
     REQUIRED_VARS GeographicLib_INCLUDE_DIR GeographicLib_LIBRARY
-    VERSION_VAR GeographicLib_VERSION)
+    FOUND_VAR GeographicLib_FOUND
+    VERSION_VAR GeographicLib_VERSION_STRING)
 
 if(GeographicLib_FOUND AND NOT TARGET GeographicLib::GeographicLib)
-    add_library(GeographicLib::GeographicLib UNKNOWN IMPORTED GLOBAL)
+    add_library(GeographicLib::GeographicLib UNKNOWN IMPORTED)
     set_target_properties(GeographicLib::GeographicLib PROPERTIES
         IMPORTED_LOCATION "${GeographicLib_LIBRARY}"
         INTERFACE_INCLUDE_DIRECTORIES "${GeographicLib_INCLUDE_DIR}")
 endif()
 
 mark_as_advanced(GeographicLib_INCLUDE_DIR GeographicLib_LIBRARY)
+set(GeographicLib_INCLUDE_DIRS ${GeographicLib_INCLUDE_DIR})
+set(GeographicLib_LIBRARIES ${GeographicLib_LIBRARY})
