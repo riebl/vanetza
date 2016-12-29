135df088a5081df52361ac30da04c46ffdfaee12
Raphael Riebl <raphael.riebl@thi.de>
2016-12-29 16:13:53 +0100

    cmake: modernise GTest integration
    Change-Id: I02a24c1586d205d3128f24e8b74606b4af6191bd

--- cmake/UseVanetza.cmake
+++ cmake/UseVanetza.cmake
@@ -13,3 +13,14 @@ function(add_vanetza_component name)
     install(TARGETS ${name} EXPORT ${PROJECT_NAME} DESTINATION ${CMAKE_INSTALL_LIBDIR})
     set_property(GLOBAL APPEND PROPERTY VANETZA_COMPONENTS ${name})
 endfunction()
+
+#
+# add_test_subdirectory(<directory>)
+#
+# Add subdirectory only when tests are enabled via BUILD_TESTS
+#
+function(add_test_subdirectory directory)
+  if(BUILD_TESTS)
+    add_subdirectory(${directory})
+  endif()
+endfunction(add_test_subdirectory)
