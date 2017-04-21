81b50c31d421bb94c095eff12a5226187629acfe
Raphael Riebl <raphael.riebl@thi.de>
2017-04-21 14:54:27 +0200

    common: add ObjectContainer
    Change-Id: I8b94cb6ded8077465bf05a2ee39cd061b601e340

---
+++ vanetza/common/object_container.hpp
@@ -0,0 +1,95 @@
+#ifndef OBJECT_CONTAINER_HPP_25SOHVUH
+#define OBJECT_CONTAINER_HPP_25SOHVUH
+
+#include <functional>
+#include <map>
+#include <memory>
+#include <typeindex>
+#include <type_traits>
+
+namespace vanetza
+{
+
+class ObjectContainer
+{
+private:
+    struct object_handle
+    {
+        template<typename T>
+        object_handle(std::unique_ptr<T> obj) :
+            object(obj.release()),
+            deleter([](void* ptr) { std::default_delete<T>()(static_cast<T*>(ptr)); })
+        {
+        }
+
+        ~object_handle() {
+            deleter(object);
+            object = nullptr;
+        }
+
+        void* object;
+        std::function<void(void*)> deleter;
+    };
+    using container_type = std::map<std::type_index, object_handle>;
+
+public:
+    ObjectContainer() = default;
+
+    // no copy
+    ObjectContainer(const ObjectContainer&) = delete;
+    ObjectContainer& operator=(const ObjectContainer&) = delete;
+
+    // allow move
+    ObjectContainer(ObjectContainer&&) = default;
+    ObjectContainer& operator=(ObjectContainer&&) = default;
+
+    bool empty() const { return m_container.empty(); }
+
+    std::size_t size() const { return m_container.size(); }
+
+    void clear() { m_container.clear(); }
+
+    template<typename T>
+    void erase()
+    {
+        m_container.erase(std::type_index(typeid(T)));
+    }
+
+    template<typename T>
+    T* find()
+    {
+        T* result = nullptr;
+        auto found = m_container.find(std::type_index(typeid(T)));
+        if (found != m_container.end()) {
+            result = static_cast<T*>(found->second.object);
+        }
+        return result;
+    }
+
+    template<typename T>
+    const T* find() const
+    {
+        const T* result = nullptr;
+        auto found = m_container.find(std::type_index(typeid(T)));
+        if (found != m_container.end()) {
+            result = static_cast<const T*>(found->second.object);
+        }
+        return result;
+    }
+
+    template<typename T>
+    bool insert(std::unique_ptr<T> obj)
+    {
+        static_assert(std::is_object<T>() && !std::is_const<T>(),
+                "Only non-const objects are supported by ObjectContainer");
+        return m_container.emplace(std::type_index(typeid(T)), std::move(obj)).second;
+    }
+
+private:
+    container_type m_container;
+};
+
+} // namespace vanetza
+
+#endif /* OBJECT_CONTAINER_HPP_25SOHVUH */
+
---
+++ vanetza/common/tests/object_container.cpp
@@ -0,0 +1,109 @@
+#include <gtest/gtest.h>
+#include <vanetza/common/object_container.hpp>
+
+using namespace vanetza;
+
+struct ObjectA
+{
+    int a = 1;
+};
+
+struct ObjectB
+{
+    int b = 2;
+};
+
+struct ObjectC
+{
+    int c = 3;
+};
+
+template<typename T>
+std::unique_ptr<T> create_unique()
+{
+    return std::unique_ptr<T> { new T() };
+}
+
+
+TEST(ObjectContainer, size)
+{
+    ObjectContainer c;
+    EXPECT_EQ(0, c.size());
+
+    c.insert(create_unique<ObjectA>());
+    EXPECT_EQ(1, c.size());
+
+    c.insert(create_unique<ObjectA>());
+    EXPECT_EQ(1, c.size());
+
+    c.insert(create_unique<ObjectB>());
+    EXPECT_EQ(2, c.size());
+
+    c.clear();
+    EXPECT_EQ(0, c.size());
+}
+
+TEST(ObjectContainer, insert)
+{
+    ObjectContainer c;
+    EXPECT_TRUE(c.insert(create_unique<ObjectA>()));
+    EXPECT_FALSE(c.insert(create_unique<ObjectA>()));
+    EXPECT_TRUE(c.insert(create_unique<ObjectB>()));
+}
+
+TEST(ObjectContainer, find)
+{
+    ObjectContainer c;
+    EXPECT_EQ(nullptr, c.find<ObjectA>());
+
+    auto a = create_unique<ObjectA>();
+    ObjectA* pa = a.get();
+    auto b = create_unique<ObjectB>();
+    ObjectB* pb = b.get();
+
+    c.insert(std::move(a));
+    c.insert(std::move(b));
+    EXPECT_EQ(pa, c.find<ObjectA>());
+    EXPECT_EQ(pb, c.find<ObjectB>());
+}
+
+TEST(ObjectContainer, erase)
+{
+    ObjectContainer c;
+    c.insert(create_unique<ObjectA>());
+    ASSERT_EQ(1, c.size());
+    c.erase<ObjectB>();
+    EXPECT_EQ(1, c.size());
+    c.erase<ObjectA>();
+    EXPECT_EQ(0, c.size());
+
+    c.insert(create_unique<ObjectA>());
+    c.insert(create_unique<ObjectB>());
+    c.insert(create_unique<ObjectC>());
+    c.erase<ObjectB>();
+    EXPECT_EQ(2, c.size());
+    EXPECT_NE(nullptr, c.find<ObjectA>());
+    EXPECT_EQ(nullptr, c.find<ObjectB>());
+    EXPECT_NE(nullptr, c.find<ObjectC>());
+}
+
+TEST(ObjectContainer, move)
+{
+    ObjectContainer c1;
+    c1.insert(create_unique<ObjectA>());
+    c1.insert(create_unique<ObjectB>());
+    c1.insert(create_unique<ObjectC>());
+    ASSERT_EQ(3, c1.size());
+
+    auto* pa = c1.find<ObjectA>();
+    auto* pb = c1.find<ObjectB>();
+    auto* pc = c1.find<ObjectC>();
+
+    ObjectContainer c2 = std::move(c1);
+    EXPECT_EQ(0, c1.size());
+    EXPECT_EQ(3, c2.size());
+    EXPECT_EQ(pa, c2.find<ObjectA>());
+    EXPECT_EQ(pb, c2.find<ObjectB>());
+    EXPECT_EQ(pc, c2.find<ObjectC>());
+}
+
