7e6fb23e40d656f0739d5fe99145964ec1304f71
Niklas Keller <me@kelunik.com>
2018-02-04 21:35:42 +0100

    travis: Fix Makefile.personal copy instruction

--- travis/titan.sh
+++ travis/titan.sh
@@ -1,9 +1,9 @@
 set -e
 
 git clone https://github.com/eclipse/titan.core.git $HOME/titan.core
-cd $HOME/titan.core
 
 cp travis/titan-makefile $HOME/titan.core/Makefile.personal
+cd $HOME/titan.core
 
 make -j4
 make install
