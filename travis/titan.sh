b9fd61d50bcce171227c2103cd72464bdcd13fd6
Niklas Keller <me@kelunik.com>
2018-02-04 21:27:13 +0100

    travis: Add Makefile.personal for Titan with correct paths

--- travis/titan.sh
+++ travis/titan.sh
@@ -2,5 +2,8 @@ set -e
 
 git clone https://github.com/eclipse/titan.core.git $HOME/titan.core
 cd $HOME/titan.core
+
+cp travis/titan-makefile $HOME/titan.core/Makefile.personal
+
 make -j4
 make install
