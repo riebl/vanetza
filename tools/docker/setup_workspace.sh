c2f77a5648d77b20f027f843a9a4db4eca73808c
Raphael Riebl <raphael.riebl@thi.de>
2018-06-17 14:15:23 +0200

    Travis: use bind mount to eliminate repository clone in container
    Change-Id: I72a6f9afe42f7f3d5ba2a7c23391f983feb8642d

--- tools/docker/setup_workspace.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-REPO=${1:?missing git repository}
-BRANCH=${2:-master}
-COMMIT=${3:-HEAD}
-
-git clone --depth=25 --branch=${BRANCH} $REPO vanetza
-cd vanetza
-git checkout ${COMMIT}
