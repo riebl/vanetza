3b9142e3af7471122c43068bd7c1e100d9f23f97
Raphael Riebl <raphael.riebl@thi.de>
2018-09-25 16:44:35 +0200

    asn1: fix missing line break after comment line when collecting source files
    Change-Id: If21a1f56dade6d1ae5109b0814f5c24007a328ab

--- vanetza/asn1/collect_asn1c_sources.cmake
+++ vanetza/asn1/collect_asn1c_sources.cmake
@@ -7,7 +7,7 @@ endif()
 file(GLOB _sources RELATIVE ${CMAKE_CURRENT_LIST_DIR} ${ASN1C_OUTPUT_DIR}/*.c)
 list(REMOVE_ITEM _sources ${ASN1C_OUTPUT_DIR}/converter-sample.c ${ASN1C_OUTPUT_DIR}/converter-example.c)
 
-file(WRITE "${ASN1C_SOURCE_FILE}" "# generated file")
+file(WRITE "${ASN1C_SOURCE_FILE}" "# generated file\n")
 foreach(_source ${_sources})
     file(APPEND "${ASN1C_SOURCE_FILE}" "${_source}\n")
 endforeach()
