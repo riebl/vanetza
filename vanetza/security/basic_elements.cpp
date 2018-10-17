51d3f3b304e56b2342118f5da4d70eebbb11f6fe
Raphael Riebl <raphael.riebl@thi.de>
2018-10-17 18:25:48 +0200

    security: fix type narrowing warning in convert_time functions
    Change-Id: I7757a5b7af399b2a383b0074cfa7e979e53c2242

--- vanetza/security/basic_elements.cpp
+++ vanetza/security/basic_elements.cpp
@@ -18,13 +18,15 @@ HashedId3 truncate(const HashedId8& in)
 
 Time32 convert_time32(const Clock::time_point& tp)
 {
-    using namespace std::chrono;
+    using std::chrono::duration_cast;
+    using seconds = std::chrono::duration<Time32>;
     return duration_cast<seconds>(tp.time_since_epoch()).count();
 }
 
 Time64 convert_time64(const Clock::time_point& tp)
 {
-    using namespace std::chrono;
+    using std::chrono::duration_cast;
+    using microseconds = std::chrono::duration<Time64, std::micro>;
     return duration_cast<microseconds>(tp.time_since_epoch()).count();
 }
 
