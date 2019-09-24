bcf2b21a96851b09ccf03ebd5799b5f1af1c4711
Raphael Riebl <raphael.riebl@thi.de>
2019-09-24 11:44:56 +0200

    docker: script tolerates existing build directory
    Change-Id: Ifb38afd40ea61fd66628014415d3546623ff3fe8

--- tools/docker/build_and_run_tests.sh
+++ tools/docker/build_and_run_tests.sh
@@ -5,7 +5,7 @@ SCRIPT_PATH=$(dirname $SCRIPT)
 ROOT=${1:-$SCRIPT_PATH/../..}
 ROOT_PATH=$(readlink -f ${ROOT})
 
-mkdir build && cd build
+mkdir -p build && cd build
 cmake -DBUILD_TESTS=ON ${ROOT_PATH}
 cmake --build .
 ctest
