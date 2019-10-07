7e2a2809abf55086f786105d54436b5cf3c7ca8a
glmax <glmax@users.noreply.github.com>
2019-10-07 19:35:30 +0000

    access: use actual 802.11 user priority values for enum AccessCategory

--- vanetza/access/access_category.hpp
+++ vanetza/access/access_category.hpp
@@ -8,11 +8,12 @@ namespace vanetza
 namespace access
 {
 
+/* See ETSI EN 302 663 V1.2.1 (2013-07), Table B.3 */
 enum class AccessCategory {
-    BK = 0,
-    BE = 1,
-    VI = 2,
-    VO = 3
+    BK = 1,
+    BE = 3,
+    VI = 5,
+    VO = 7
 };
 
 std::ostream& operator<<(std::ostream&, AccessCategory);
