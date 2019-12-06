ba6ee5068e1c70e10c2ae07a1ea980808551f93d
Raphael Riebl <raphael.riebl@thi.de>
2019-12-06 14:30:08 +0100

    improve documentation of BitNumber
    Change-Id: I5d178342ad03d8418bd3c96d4024a50c6a74b8b8

--- vanetza/common/bit_number.hpp
+++ vanetza/common/bit_number.hpp
@@ -7,6 +7,12 @@
 
 namespace vanetza {
 
+/**
+ * BitNumber restricts the value range by the given width
+ *
+ * \tparam T underlying integral type
+ * \tparam WIDTH number of bits used to represent a number
+ */
 template<typename T, std::size_t WIDTH>
 class BitNumber : public boost::totally_ordered<BitNumber<T, WIDTH>>
 {
@@ -16,9 +22,9 @@ class BitNumber : public boost::totally_ordered<BitNumber<T, WIDTH>>
             "width has to be less than size of underlying type");
 
 public:
-    static constexpr T mask = (1 << WIDTH) - 1;
-    static constexpr std::size_t bits = WIDTH;
-    typedef T value_type;
+    static constexpr T mask = (1 << WIDTH) - 1; /**< excessive bits are masked */
+    static constexpr std::size_t bits = WIDTH; /**< number of bits used */
+    typedef T value_type; /**< underlying (integral) value type */
 
     BitNumber() : mValue(0) {}
     BitNumber(T value) : mValue(value & mask) {}
