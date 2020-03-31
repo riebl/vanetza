ccf8e50b16dd42e5864d9343efaca2df45d99973
Raphael Riebl <raphael.riebl@thi.de>
2020-03-31 11:03:58 +0200

    asn1: add CPM wrapper
    Change-Id: Ibd3634b4acb3034b82b63f5219f096a840be8cff

---
+++ vanetza/asn1/cpm.hpp
@@ -0,0 +1,23 @@
+#ifndef CPM_HPP_WEWZK69S
+#define CPM_HPP_WEWZK69S
+
+#include <vanetza/asn1/asn1c_conversion.hpp>
+#include <vanetza/asn1/asn1c_wrapper.hpp>
+#include <vanetza/asn1/its/CPM.h>
+
+namespace vanetza
+{
+namespace asn1
+{
+
+class Cpm : public asn1c_wrapper<CPM_t>
+{
+public:
+    Cpm() : asn1c_wrapper(asn_DEF_CPM) {}
+};
+
+} // namespace asn1
+} // namespace vanetza
+
+#endif /* CPM_HPP_WEWZK69S */
+
