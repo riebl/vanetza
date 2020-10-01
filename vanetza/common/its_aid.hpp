94d68a4789ac61d83562dfbeff34726267ce9137
Keno Garlichs <garlichs@ibr.cs.tu-bs.de>
2020-10-01 11:01:08 +0200

    Update ITS-AIDs according to TS 102 965 v1.5.1

--- vanetza/common/its_aid.hpp
+++ vanetza/common/its_aid.hpp
@@ -14,15 +14,23 @@ namespace aid
 
 /**
  * ITS-AID assigned for ETSI ITS
- * \see TS 102 965 V1.3.1 Annex A
+ * \see TS 102 965 V1.5.1 Annex A
  */
 constexpr ItsAid CA = 36;
 constexpr ItsAid DEN = 37;
 constexpr ItsAid TLM = 137;
 constexpr ItsAid RLT = 138;
 constexpr ItsAid IVI = 139;
-constexpr ItsAid TLC = 140;
+constexpr ItsAid TLC_R = 140;
+constexpr ItsAid TLC_S = 637;
 constexpr ItsAid GN_MGMT = 141;
+constexpr ItsAid CRL = 622;
+constexpr ItsAid SCR = 623;
+constexpr ItsAid CTL = 624;
+constexpr ItsAid VRU = 638;
+constexpr ItsAid CP = 639;
+constexpr ItsAid SA = 540801;
+constexpr ItsAid GPC = 540802;
 constexpr ItsAid IPV6_ROUTING = 270549118;
 
 } // namespace aid
