5b188e2f973a99bd9435623e4f8e0f7ba1cc851a
Raphael Riebl <raphael.riebl@thi.de>
2020-10-29 16:30:01 +0100

    update Travis CI configuration
    Change-Id: I7b4acd2f38d94c6bb866bb2c5b88af9b72d6f937

--- tools/docker/build_and_run_tests.sh
+++ tools/docker/build_and_run_tests.sh
@@ -4,8 +4,9 @@ SCRIPT=$(readlink -f $0)
 SCRIPT_PATH=$(dirname $SCRIPT)
 ROOT=${1:-$SCRIPT_PATH/../..}
 ROOT_PATH=$(readlink -f ${ROOT})
+BUILD_DIR=${BUILD_DIR:-$PWD/build}
 
-mkdir -p build && cd build
-cmake -DBUILD_TESTS=ON ${ROOT_PATH}
+mkdir -p ${BUILD_DIR} && cd ${BUILD_DIR}
+cmake -DBUILD_TESTS=ON -DGTest_BUILD_DIRECTORY_DOWNLOAD=ON ${ROOT_PATH}
 cmake --build .
 ctest
