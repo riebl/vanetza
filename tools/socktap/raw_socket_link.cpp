riebl/vanetza

commit f397ab208bb3f500fc99c306fb0436d016c0dda1

socktap: add LinkLayer abstraction
Change-Id: I0a32c9023725ac687735cda72cac8afeca8cb1ac

tools/socktap/raw_socket_link.cpp
@@ -1,0 +1,71 @@
+#include "raw_socket_link.hpp"
+#include <vanetza/access/data_request.hpp>
+#include <vanetza/net/ethernet_header.hpp>
+#include <iostream>
+
+using namespace vanetza;
+
+RawSocketLink::RawSocketLink(boost::asio::generic::raw_protocol::socket&& socket) :
+    socket_(std::move(socket)), receive_buffer_(2048, 0x00),
+    receive_endpoint_(socket_.local_endpoint())
+{
+    do_receive();
+}
+
+void RawSocketLink::request(const access::DataRequest& request, std::unique_ptr<ChunkPacket> packet)
+{
+    packet->layer(OsiLayer::Link) = create_ethernet_header(request.destination_addr, request.source_addr, request.ether_type);
+    transmit(std::move(packet));
+}
+
+std::size_t RawSocketLink::transmit(std::unique_ptr<ChunkPacket> packet)
+{
+    std::array<boost::asio::const_buffer, layers_> const_buffers;
+    for (auto& layer : osi_layer_range<OsiLayer::Link, OsiLayer::Application>()) {
+        const auto index = distance(OsiLayer::Link, layer);
+        packet->layer(layer).convert(buffers_[index]);
+        const_buffers[index] = boost::asio::buffer(buffers_[index]);
+    }
+
+    return socket_.send(const_buffers);
+}
+
+void RawSocketLink::indicate(IndicationCallback callback)
+{
+    callback_ = callback;
+}
+
+void RawSocketLink::do_receive()
+{
+    namespace sph = std::placeholders;
+    socket_.async_receive_from(
+            boost::asio::buffer(receive_buffer_), receive_endpoint_,
+            std::bind(&RawSocketLink::on_read, this, sph::_1, sph::_2));
+}
+
+void RawSocketLink::on_read(const boost::system::error_code& ec, std::size_t read_bytes)
+{
+    if (!ec) {
+        ByteBuffer buffer(receive_buffer_.begin(), receive_buffer_.begin() + read_bytes);
+        CohesivePacket packet(std::move(buffer), OsiLayer::Physical);
+        boost::optional<EthernetHeader> eth = parse_ethernet_header(packet);
+        if (callback_ && eth) {
+            callback_(std::move(packet), *eth);
+        }
+        do_receive();
+    }
+}
+
+boost::optional<EthernetHeader> RawSocketLink::parse_ethernet_header(vanetza::CohesivePacket& packet) const
+{
+    packet.set_boundary(OsiLayer::Physical, 0);
+    if (packet.size(OsiLayer::Link) < EthernetHeader::length_bytes) {
+        std::cerr << "Router dropped invalid packet (too short for Ethernet header)\n";
+    } else {
+        packet.set_boundary(OsiLayer::Link, EthernetHeader::length_bytes);
+        auto link_range = packet[OsiLayer::Link];
+        return decode_ethernet_header(link_range.begin(), link_range.end());
+    }
+
+    return boost::none;
+}

tools/socktap/raw_socket_link.hpp
@@ -1,0 +1,38 @@
+#ifndef RAW_SOCKET_LINK_HPP_VUXH507U
+#define RAW_SOCKET_LINK_HPP_VUXH507U
+
+#include "link_layer.hpp"
+#include <vanetza/access/interface.hpp>
+#include <vanetza/net/ethernet_header.hpp>
+#include <boost/asio/generic/raw_protocol.hpp>
+#include <boost/optional/optional.hpp>
+#include <array>
+#include <functional>
+
+class RawSocketLink : public LinkLayer
+{
+public:
+    RawSocketLink(boost::asio::generic::raw_protocol::socket&&);
+    void request(const vanetza::access::DataRequest&, std::unique_ptr<vanetza::ChunkPacket>) override;
+    void indicate(IndicationCallback) override;
+
+protected:
+    std::size_t transmit(std::unique_ptr<vanetza::ChunkPacket>);
+    virtual boost::optional<vanetza::EthernetHeader> parse_ethernet_header(vanetza::CohesivePacket&) const;
+
+private:
+    void do_receive();
+    void on_read(const boost::system::error_code&, std::size_t);
+    void pass_up(vanetza::CohesivePacket&&);
+
+    static constexpr std::size_t layers_ = num_osi_layers(vanetza::OsiLayer::Link, vanetza::OsiLayer::Application);
+
+    boost::asio::generic::raw_protocol::socket socket_;
+    std::array<vanetza::ByteBuffer, layers_> buffers_;
+    IndicationCallback callback_;
+    vanetza::ByteBuffer receive_buffer_;
+    boost::asio::generic::raw_protocol::endpoint receive_endpoint_;
+};
+
+#endif /* RAW_SOCKET_LINK_HPP_VUXH507U */
+

tools/socktap/CMakeLists.txt
@@ -16,6 +16,8 @@ add_library(socktap-common STATIC
     dcc_passthrough.cpp
     ethernet_device.cpp
     gps_position_provider.cpp
+    link_layer.cpp
+    raw_socket_link.cpp
     router_context.cpp
     time_trigger.cpp
 )
@@ -43,5 +45,5 @@ if(SOCKTAP_WITH_COHDA_LLC)
     find_package(Cohda MODULE REQUIRED)
     target_compile_definitions(socktap-common PUBLIC "SOCKTAP_WITH_COHDA_LLC")
     target_include_directories(socktap-common PUBLIC ${COHDA_INCLUDE_DIRS})
-    target_sources(socktap-common PRIVATE cohda.cpp)
+    target_sources(socktap-common PRIVATE cohda.cpp cohda_link.cpp)
 endif()

tools/socktap/cohda.cpp
@@ -9,13 +9,13 @@
 namespace vanetza
 {
 
-void insert_cohda_tx_header(const dcc::DataRequest& request, std::unique_ptr<ChunkPacket>& packet)
+void insert_cohda_tx_header(const access::DataRequest& request, std::unique_ptr<ChunkPacket>& packet)
 {
     access::G5LinkLayer link_layer;
     access::ieee802::dot11::QosDataHeader& mac_header = link_layer.mac_header;
-    mac_header.destination = request.destination;
-    mac_header.source = request.source;
-    mac_header.qos_control.user_priority(dcc::map_profile_onto_ac(request.dcc_profile));
+    mac_header.destination = request.destination_addr;
+    mac_header.source = request.source_addr;
+    mac_header.qos_control.user_priority(request.access_category);
 
     ByteBuffer link_layer_buffer;
     serialize_into_buffer(link_layer, link_layer_buffer);
@@ -31,7 +31,7 @@ void insert_cohda_tx_header(const dcc::DataRequest& request, std::unique_ptr<Chu
     phy.TxPacketData.TxAntenna = MKX_ANT_DEFAULT;
     phy.TxPacketData.TxFrameLength = payload_size;
     auto phy_ptr = reinterpret_cast<const uint8_t*>(&phy);
-    packet->layer(OsiLayer::Physical) = std::move(ByteBuffer(phy_ptr, phy_ptr + sizeof(tMKxTxPacket)));
+    packet->layer(OsiLayer::Physical) = ByteBuffer { phy_ptr, phy_ptr + sizeof(tMKxTxPacket) };
 }
 
 boost::optional<EthernetHeader> strip_cohda_rx_header(CohesivePacket& packet)

tools/socktap/cohda.hpp
@@ -2,21 +2,26 @@
 #define COHDA_HPP_GBENHCVN
 
 #include <boost/optional/optional.hpp>
+#include <vanetza/access/data_request.hpp>
 #include <vanetza/common/byte_buffer.hpp>
-#include <vanetza/net/chunk_packet.hpp>
 #include <vanetza/common/clock.hpp>
-#include <vanetza/dcc/data_request.hpp>
-#include <vanetza/net/ethernet_header.hpp>
+#include <vanetza/net/chunk_packet.hpp>
 #include <vanetza/net/cohesive_packet.hpp>
 
 namespace vanetza
 {
 
-// forward declaration
-namespace dcc { class DataRequest; }
+/**
+ * Add Physical and Link layer headers understood by Cohda V2X API
+ * \param req access layer request parameters
+ * \param packet packet to be transmitted
+ */
+void insert_cohda_tx_header(const access::DataRequest& req, std::unique_ptr<ChunkPacket>& packet);
 
-void insert_cohda_tx_header(const dcc::DataRequest&, std::unique_ptr<ChunkPacket>&);
-
+/**
+ * Remove packet headers by Cohda V2X API and build Ethernet header from them
+ * \return equivalent EthernetHeader if successfully received
+ */
 boost::optional<EthernetHeader> strip_cohda_rx_header(CohesivePacket&);
 
 } // namespace vanetza

tools/socktap/cohda_link.cpp
@@ -1,0 +1,13 @@
+#include "cohda_link.hpp"
+#include "cohda.hpp"
+
+void CohdaLink::request(const vanetza::access::DataRequest& request, std::unique_ptr<vanetza::ChunkPacket> packet)
+{
+    insert_cohda_tx_header(request, packet);
+    transmit(std::move(packet));
+}
+
+boost::optional<vanetza::EthernetHeader> CohdaLink::parse_ethernet_header(vanetza::CohesivePacket& packet) const
+{
+    return strip_cohda_rx_header(packet);
+}

tools/socktap/cohda_link.hpp
@@ -1,0 +1,18 @@
+#ifndef COHDA_LINK_HPP_IXOCQ5RH
+#define COHDA_LINK_HPP_IXOCQ5RH
+
+#include "raw_socket_link.hpp"
+
+class CohdaLink : public RawSocketLink
+{
+public:
+    using RawSocketLink::RawSocketLink;
+
+    void request(const vanetza::access::DataRequest&, std::unique_ptr<vanetza::ChunkPacket>) override;
+
+protected:
+    boost::optional<vanetza::EthernetHeader> parse_ethernet_header(vanetza::CohesivePacket&) const override;
+};
+
+#endif /* COHDA_LINK_HPP_IXOCQ5RH */
+

tools/socktap/dcc_passthrough.cpp
@@ -1,22 +1,16 @@
 #include "dcc_passthrough.hpp"
 #include "time_trigger.hpp"
+#include <vanetza/access/data_request.hpp>
 #include <vanetza/dcc/data_request.hpp>
 #include <vanetza/dcc/interface.hpp>
-#include <vanetza/net/ethernet_header.hpp>
+#include <vanetza/dcc/mapping.hpp>
 #include <vanetza/net/chunk_packet.hpp>
-#include <boost/asio/generic/raw_protocol.hpp>
 #include <iostream>
 
-#ifdef SOCKTAP_WITH_COHDA_LLC
-#include "cohda.hpp"
-#endif
-
-namespace asio = boost::asio;
-using boost::asio::generic::raw_protocol;
 using namespace vanetza;
 
-DccPassthrough::DccPassthrough(raw_protocol::socket& socket, TimeTrigger& trigger) :
-        socket_(socket), trigger_(trigger) {}
+DccPassthrough::DccPassthrough(access::Interface& access, TimeTrigger& trigger) :
+        access_(access), trigger_(trigger) {}
 
 
 void DccPassthrough::request(const dcc::DataRequest& request, std::unique_ptr<ChunkPacket> packet)
@@ -26,24 +20,14 @@ void DccPassthrough::request(const dcc::DataRequest& request, std::unique_ptr<Ch
         return;
     }
 
-#ifdef SOCKTAP_WITH_COHDA_LLC
-    insert_cohda_tx_header(request, packet);
-#else
-    packet->layer(OsiLayer::Link) = create_ethernet_header(request.destination, request.source, request.ether_type);
-#endif
-    for (auto& layer : osi_layer_range<OsiLayer::Physical, OsiLayer::Application>()) {
-        const auto index = distance(OsiLayer::Physical, layer);
-        packet->layer(layer).convert(buffers_[index]);
-    }
-
     trigger_.schedule();
 
-    std::array<asio::const_buffer, layers_> const_buffers;
-    for (unsigned i = 0; i < const_buffers.size(); ++i) {
-        const_buffers[i] = asio::buffer(buffers_[i]);
-    }
-    auto bytes_sent = socket_.send(const_buffers);
-    std::cout << "sent packet to " << request.destination << " (" << bytes_sent << " bytes)\n";
+    access::DataRequest acc_req;
+    acc_req.ether_type = request.ether_type;
+    acc_req.source_addr = request.source;
+    acc_req.destination_addr = request.destination;
+    acc_req.access_category = dcc::map_profile_onto_ac(request.dcc_profile);
+    access_.request(acc_req, std::move(packet));
 }
 
 void DccPassthrough::allow_packet_flow(bool allow)

commit 437222b9fe4b8f51adbe06aace164431a610c045

socktap: add Physical layer to layers send over raw_socket_link
Fixes issue to send over Cohda link-layer introduced in f397ab2

tools/socktap/raw_socket_link.cpp
@@ -21,8 +21,8 @@ void RawSocketLink::request(const access::DataRequest& request, std::unique_ptr<
 std::size_t RawSocketLink::transmit(std::unique_ptr<ChunkPacket> packet)
 {
     std::array<boost::asio::const_buffer, layers_> const_buffers;
-    for (auto& layer : osi_layer_range<OsiLayer::Link, OsiLayer::Application>()) {
-        const auto index = distance(OsiLayer::Link, layer);
+    for (auto& layer : osi_layer_range<OsiLayer::Physical, OsiLayer::Application>()) {
+        const auto index = distance(OsiLayer::Physical, layer);
         packet->layer(layer).convert(buffers_[index]);
         const_buffers[index] = boost::asio::buffer(buffers_[index]);
     }

tools/socktap/raw_socket_link.hpp
@@ -25,7 +25,7 @@ private:
     void on_read(const boost::system::error_code&, std::size_t);
     void pass_up(vanetza::CohesivePacket&&);
 
-    static constexpr std::size_t layers_ = num_osi_layers(vanetza::OsiLayer::Link, vanetza::OsiLayer::Application);
+    static constexpr std::size_t layers_ = num_osi_layers(vanetza::OsiLayer::Physical, vanetza::OsiLayer::Application);
 
     boost::asio::generic::raw_protocol::socket socket_;
     std::array<vanetza::ByteBuffer, layers_> buffers_;