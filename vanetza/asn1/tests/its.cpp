26c7dd24b447a5bff8a8880ea545b75419d11cef
Raphael Riebl <raphael@riebl.net>
2022-02-27 20:04:38 +0100

    asn1: move ITS test cases to ItsAsn1 test suite
    Change-Id: I9f647991e49153a3081e06e988453d924bc245e4

--- vanetza/asn1/tests/its.cpp
+++ vanetza/asn1/tests/its.cpp
@@ -1,10 +1,12 @@
 #include <gtest/gtest.h>
 #include <vanetza/asn1/asn1c_wrapper.hpp>
+#include <vanetza/asn1/cam.hpp>
+#include <vanetza/asn1/denm.hpp>
 #include <vanetza/asn1/its/TimestampIts.h>
 
 using namespace vanetza;
 
-TEST(ItsAsn1, TimestampIts)
+TEST(ItsAsn1, max_timestamp_roundtrip)
 {
     const std::int64_t max_timestamp = 4398046511103;
     asn1::asn1c_wrapper<TimestampIts_t> tx { asn_DEF_TimestampIts };
@@ -16,5 +18,25 @@ TEST(ItsAsn1, TimestampIts)
     std::int64_t rx_timestamp = 0;
     asn_INTEGER2imax(&*tx, &rx_timestamp);
     EXPECT_EQ(rx_timestamp, max_timestamp);
+}
+
+TEST(ItsAsn1, encode_denm)
+{
+    asn1::Denm denm;
+    EXPECT_EQ(0, asn_uint642INTEGER(&denm->denm.management.detectionTime, TimestampIts_utcStartOf2004));
+    EXPECT_EQ(0, asn_uint642INTEGER(&denm->denm.management.referenceTime, TimestampIts_utcStartOf2004));
+    EXPECT_TRUE(denm.validate());
+    vanetza::ByteBuffer buf = denm.encode();
+    EXPECT_EQ(40, buf.size());
+}
+
+TEST(ItsAsn1, create_cam)
+{
+    asn1::Cam cam;
+}
+
+TEST(ItsAsn1, create_denm)
+{
+    asn1::Denm denm;
 }
 
