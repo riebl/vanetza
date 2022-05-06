21adcef77235ae6e00b171e098d7f2c810e9cf53
karel.hevessy <karel.hevessy@machsystems.cz>
2022-05-06 11:03:16 +0200

    Added IVIM messages

--- vanetza/asn1/tests/its.cpp
+++ vanetza/asn1/tests/its.cpp
@@ -4,6 +4,9 @@
 #include <vanetza/asn1/denm.hpp>
 #include <vanetza/asn1/mapem.hpp>
 #include <vanetza/asn1/spatem.hpp>
+#include <vanetza/asn1/ssem.hpp>
+#include <vanetza/asn1/srem.hpp>
+#include <vanetza/asn1/ivim.hpp>
 #include <vanetza/asn1/its/TimestampIts.h>
 
 using namespace vanetza;
@@ -51,3 +54,9 @@ TEST(ItsAsn1, create_spatem)
 {
     asn1::Spatem spatem;
 }
+
+TEST(ItsAsn1, create_ivim)
+{
+    asn1::Ivim ivim;
+}
+
