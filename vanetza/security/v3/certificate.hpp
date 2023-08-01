96edc4cbef3c5567550477f588101ed855f5f0f9
Raphael Riebl <raphael@riebl.net>
2023-08-01 23:54:12 +0200

    security: add helper functions for ASN.1 certificate

--- vanetza/security/v3/certificate.hpp
+++ vanetza/security/v3/certificate.hpp
@@ -1,6 +1,10 @@
 #pragma once
 #include <vanetza/asn1/asn1c_wrapper.hpp>
 #include <vanetza/asn1/security/EtsiTs103097Certificate.h>
+#include <vanetza/common/its_aid.hpp>
+#include <vanetza/security/hashed_id.hpp>
+#include <vanetza/security/public_key.hpp>
+#include <boost/optional/optional_fwd.hpp>
 
 namespace vanetza
 {
@@ -12,15 +16,29 @@ namespace v3
 struct Certificate : public asn1::asn1c_oer_wrapper<EtsiTs103097Certificate_t>
 {
     Certificate();
-
 };
 
 /**
  * Calculate hash id (digest) of v3 certificate
  * \param cert certificate
- * \return hash id
+ * \return hash id if possible
  */
-HashedId8 calculate_hash(const Certificate& cert);
+boost::optional<HashedId8> calculate_hash(const EtsiTs103097Certificate_t& cert);
+
+/**
+ * Extract the public key out of a certificate
+ * \param cert certificate
+ * \return public key if possible
+ */
+boost::optional<PublicKey> get_public_key(const EtsiTs103097Certificate_t& cert);
+
+/**
+ * Get application permissions (SSP = service specific permissions)
+ * \param cert certificate containing application permissions
+ * \param aid look up permissions for this application identifier
+ * \return SSP bitmap or empty buffer
+ */
+ByteBuffer get_app_permissions(const EtsiTs103097Certificate_t& cert, ItsAid aid);
 
 } // namespace v3
 } // namespace security
