da0f0095d5974753940ad3296dea3cabd9f5cf2d
Raphael Riebl <raphael@riebl.net>
2023-08-01 23:44:53 +0200

    security: add HashedId8 conversion from ASN.1 type

---
+++ vanetza/security/v3/asn1_conversions.cpp
@@ -0,0 +1,21 @@
+#include <vanetza/security/v3/asn1_conversions.hpp>
+#include <algorithm>
+#include <cstring>
+
+namespace vanetza
+{
+namespace security
+{
+namespace v3
+{
+
+HashedId8 convert(const HashedId8_t& in)
+{
+    HashedId8 out;
+    std::memcpy(out.data(), in.buf, std::min(out.size(), in.size));
+    return out;
+}
+
+} // namespace v3
+} // namespace security
+} // namespace vanetza
---
+++ vanetza/security/v3/asn1_conversions.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <vanetza/asn1/security/HashedId8.h>
+#include <vanetza/security/hashed_id.hpp>
+
+
+namespace vanetza
+{
+namespace security
+{
+namespace v3
+{
+
+HashedId8 convert(const HashedId8_t&);
+
+} // namespace v3
+} // namespace security
+} // namespace vanetza
