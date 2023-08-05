d9f60bc0abf7c75df0607763499e2c14adba899c
Raphael Riebl <raphael@riebl.net>
2023-08-05 14:38:31 +0200

    CI: add output on failure of unit tests

--- tools/docker/build_and_run_tests.sh
+++ tools/docker/build_and_run_tests.sh
@@ -9,4 +9,4 @@ BUILD_DIR=${BUILD_DIR:-$PWD/build}
 mkdir -p ${BUILD_DIR} && cd ${BUILD_DIR}
 cmake -DBUILD_TESTS=ON -DGTest_BUILD_DIRECTORY_DOWNLOAD=ON ${ROOT_PATH}
 cmake --build .
-ctest
+ctest --output-on-failure
