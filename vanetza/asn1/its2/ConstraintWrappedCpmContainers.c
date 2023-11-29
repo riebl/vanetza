160acbfb8d16cc93dbeb7085a675929dcb286438
Diogo Jesus <diogopjesus@ua.pt>
2023-11-29 17:21:57 +0000

    Add newly generated file changes with v0-e fork of asn1c.

--- vanetza/asn1/its2/ConstraintWrappedCpmContainers.c
+++ vanetza/asn1/its2/ConstraintWrappedCpmContainers.c
@@ -40,12 +40,12 @@ ConstraintWrappedCpmContainers_constraint(const asn_TYPE_descriptor_t *td, const
 #if !defined(ASN_DISABLE_OER_SUPPORT)
 static asn_oer_constraints_t asn_OER_type_ConstraintWrappedCpmContainers_constr_1 CC_NOTUSED = {
 	{ 0, 0 },
-	-1	/* (SIZE(0..MAX)) */};
+	-1	/* (SIZE(1..8)) */};
 #endif  /* !defined(ASN_DISABLE_OER_SUPPORT) */
 #if !defined(ASN_DISABLE_UPER_SUPPORT) || !defined(ASN_DISABLE_APER_SUPPORT)
 asn_per_constraints_t asn_PER_type_ConstraintWrappedCpmContainers_constr_1 CC_NOTUSED = {
 	{ APC_UNCONSTRAINED,	-1, -1,  0,  0 },
-	{ APC_CONSTRAINED | APC_EXTENSIBLE,  3,  3,  1,  8 }	/* (SIZE(1..8,...)) */,
+	{ APC_CONSTRAINED,	 3,  3,  1,  8 }	/* (SIZE(1..8)) */,
 	0, 0	/* No PER value map */
 };
 #endif  /* !defined(ASN_DISABLE_UPER_SUPPORT) || !defined(ASN_DISABLE_APER_SUPPORT) */
