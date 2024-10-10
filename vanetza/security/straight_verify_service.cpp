15c6b83e11384c2a6d374c9a9da19f5e9b3e653b
Raphael Riebl <raphael@riebl.net>
2024-10-10 17:42:59 +0000

    security: request only for received CAMs unknown ATs

--- vanetza/security/straight_verify_service.cpp
+++ vanetza/security/straight_verify_service.cpp
@@ -463,7 +463,8 @@ VerifyConfirm StraightVerifyService::verify(const v3::SecuredMessage& msg)
 
     const v3::asn1::Certificate* certificate = boost::apply_visitor(certificate_lookup_visitor, signer_identifier);
     if (!certificate && maybe_digest) {
-        if (m_context_v3.m_sign_policy) {
+        if (msg.its_aid() == aid::CA && m_context_v3.m_sign_policy) {
+            // for received CAMs (having digest as signer identifier) with unknown AT we request the full AT certificate
             m_context_v3.m_sign_policy->request_unrecognized_certificate(*maybe_digest);
         }
         confirm.report = VerificationReport::Signer_Certificate_Not_Found;
