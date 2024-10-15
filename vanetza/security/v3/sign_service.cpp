d0f5fcee025c8ea3abaddcc7623ff1ad42054f12
Raphael Riebl <raphael.riebl@nfiniity.com>
2024-10-15 12:03:19 +0000

    security: calculate message hash with canonicalized certificate

--- vanetza/security/v3/sign_service.cpp
+++ vanetza/security/v3/sign_service.cpp
@@ -58,8 +58,18 @@ SignConfirm DummySignService::sign(SignRequest&& request)
 
 ByteBuffer calculate_message_hash(Backend& backend, HashAlgorithm hash_algo, const ByteBuffer& payload, const Certificate& signing_cert)
 {
+    ByteBuffer encoded_cert;
+    if (signing_cert.is_canonical()) {
+        encoded_cert = signing_cert.encode();
+    } else {
+        auto canonical_signing_cert = signing_cert.canonicalize();
+        if (canonical_signing_cert) {
+            encoded_cert = canonical_signing_cert->encode();
+        }
+    }
+
     ByteBuffer data_hash = backend.calculate_hash(hash_algo, payload);
-    ByteBuffer cert_hash = backend.calculate_hash(hash_algo, signing_cert.encode());
+    ByteBuffer cert_hash = backend.calculate_hash(hash_algo, encoded_cert);
     ByteBuffer concat_hash;
     concat_hash.reserve(data_hash.size() + cert_hash.size());
     concat_hash.insert(concat_hash.end(), data_hash.begin(), data_hash.end());
