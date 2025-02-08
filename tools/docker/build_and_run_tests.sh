f612481a5ce7c666a3835b1cfeaec4c87d55f0c3
Raphael Riebl <raphael@riebl.net>
2025-02-08 09:38:38 +0100

    CI: add socktap and certify builds
    Fixes #244

--- tools/docker/build_and_run_tests.sh
+++ tools/docker/build_and_run_tests.sh
@@ -7,6 +7,9 @@ ROOT_PATH=$(readlink -f ${ROOT})
 BUILD_DIR=${BUILD_DIR:-$PWD/build}
 
 mkdir -p ${BUILD_DIR} && cd ${BUILD_DIR}
-cmake -G Ninja -DBUILD_TESTS=ON -DGTest_BUILD_DIRECTORY_DOWNLOAD=ON ${ROOT_PATH}
+cmake -G Ninja \
+    -DBUILD_CERTIFY=ON -DBUILD_SOCKTAP=ON \
+    -DBUILD_TESTS=ON  -DGTest_BUILD_DIRECTORY_DOWNLOAD=ON \
+    ${ROOT_PATH}
 cmake --build .
 ctest --output-on-failure
