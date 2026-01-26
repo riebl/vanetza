9929b6ace967c5bc5e3cb1a00bc27b6899224d80
Raphael Riebl <raphael@riebl.net>
2026-01-26 09:59:50 +0100

    asn1: fix patching of IEEE ScopedCertificateRequest

--- vanetza/asn1/fetch_ieee_asn1_files.cmake
+++ vanetza/asn1/fetch_ieee_asn1_files.cmake
@@ -1,7 +1,10 @@
-if(NOT DIRECTORY)
-    message(FATAL_ERROR "DIRECTORY variable is undefined")
+if(NOT OUTPUT_DIRECTORY)
+    message(FATAL_ERROR "OUTPUT_DIRECTORY variable is undefined")
 endif()
-file(MAKE_DIRECTORY "${DIRECTORY}")
+file(MAKE_DIRECTORY "${OUTPUT_DIRECTORY}")
+if(NOT PATCH_DIRECTORY)
+    message(FATAL_ERROR "PATH_DIRECTORY variable is undefined")
+endif()
 
 macro(download URL OUTPUT_FILE)
     file(DOWNLOAD "${URL}" "${OUTPUT_FILE}"
@@ -16,13 +19,13 @@ endmacro()
 macro(download_ieee1609dot2 ASN1_FILE)
     download(
         "https://forge.etsi.org/rep/ITS/asn1/ieee1609.2/-/raw/2022-published/${ASN1_FILE}"
-        "${DIRECTORY}/${ASN1_FILE}")
+        "${OUTPUT_DIRECTORY}/${ASN1_FILE}")
 endmacro()
 
 macro(download_ieee1609dot2dot1 ASN1_FILE)
     download(
         "https://forge.etsi.org/rep/ITS/asn1/ieee1609.2.1/-/raw/2022-published/${ASN1_FILE}"
-        "${DIRECTORY}/${ASN1_FILE}")
+        "${OUTPUT_DIRECTORY}/${ASN1_FILE}")
 endmacro()
 
 download_ieee1609dot2("Ieee1609Dot2.asn")
@@ -44,16 +47,16 @@ download_ieee1609dot2dot1("Ieee1609Dot2Dot1LaRaInterface.asn")
 download_ieee1609dot2dot1("Ieee1609Dot2Dot1MaRaInterface.asn")
 download_ieee1609dot2dot1("Ieee1609Dot2Dot1Protocol.asn")
 
-file(READ "${DIRECTORY}/Ieee1609Dot2.asn" _content)
+file(READ "${OUTPUT_DIRECTORY}/Ieee1609Dot2.asn" _content)
 string(REGEX REPLACE " Extension" " IeeeExtension" _content "${_content}")
-file(WRITE "${DIRECTORY}/Ieee1609Dot2.asn" "${_content}")
+file(WRITE "${OUTPUT_DIRECTORY}/Ieee1609Dot2.asn" "${_content}")
 
-file(READ "${DIRECTORY}/Ieee1609Dot2BaseTypes.asn" _content)
+file(READ "${OUTPUT_DIRECTORY}/Ieee1609Dot2BaseTypes.asn" _content)
 string(REGEX REPLACE "([\r\n])Extension" "\\1IeeeExtension" _content "${_content}")
-file(WRITE "${DIRECTORY}/Ieee1609Dot2BaseTypes.asn" "${_content}")
+file(WRITE "${OUTPUT_DIRECTORY}/Ieee1609Dot2BaseTypes.asn" "${_content}")
 
-execute_process(COMMAND patch -p1 -i "patches/ieee/scoped-certificate-request.patch"
-    WORKING_DIRECTORY "${DIRECTORY}"
+execute_process(COMMAND patch -p1 -i "${PATCH_DIRECTORY}/scoped-certificate-request.patch"
+    WORKING_DIRECTORY "${OUTPUT_DIRECTORY}"
     RESULT_VARIABLE PATCH_RESULT)
 if(NOT PATCH_RESULT EQUAL 0)
     message(SEND_ERROR "Applying patch on IEEE 1609.2.1 ScopedCertificateRequest failed!")
