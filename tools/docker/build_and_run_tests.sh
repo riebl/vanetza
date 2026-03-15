9cb6d0e513703abb4ef66b42f8c984623b1df2e1
Raphael Riebl <raphael@riebl.net>
2026-03-15 20:02:24 +0100

    CI: run tests with OpenSSL and CryptoPP enabled

--- tools/docker/build_and_run_tests.sh
+++ tools/docker/build_and_run_tests.sh
@@ -8,8 +8,12 @@ BUILD_DIR=${BUILD_DIR:-$PWD/build}
 
 mkdir -p ${BUILD_DIR} && cd ${BUILD_DIR}
 cmake -G Ninja \
-    -DBUILD_CERTIFY=ON -DBUILD_SOCKTAP=ON \
-    -DBUILD_TESTS=ON  -DGTest_BUILD_DIRECTORY_DOWNLOAD=ON \
+    -DVANETZA_WITH_CRYPTOPP=ON \
+    -DVANETZA_WITH_OPENSSL=ON \
+    -DBUILD_CERTIFY=ON \
+    -DBUILD_SOCKTAP=ON \
+    -DBUILD_TESTS=ON \
+    -DGTest_BUILD_DIRECTORY_DOWNLOAD=ON \
     ${ROOT_PATH}
 cmake --build .
 ctest --output-on-failure
