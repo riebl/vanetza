aeaac0460df7933361e8c949b20a67a215bd1e04
Raphael Riebl <raphael.riebl@nfiniity.com>
2026-03-30 12:17:30 +0200

    asn1: add smoke test for wrapper types

---
+++ vanetza/asn1/tests/smoke.cpp
@@ -0,0 +1,23 @@
+#include <vanetza/asn1/cam.hpp>
+#include <vanetza/asn1/cpm.hpp>
+#include <vanetza/asn1/denm.hpp>
+#include <vanetza/asn1/ivim.hpp>
+#include <vanetza/asn1/mapem.hpp>
+#include <vanetza/asn1/rtcmem.hpp>
+#include <vanetza/asn1/spatem.hpp>
+#include <vanetza/asn1/srem.hpp>
+#include <vanetza/asn1/ssem.hpp>
+#include <vanetza/asn1/vam.hpp>
+
+// just make sure wrappers are compilable
+static vanetza::asn1::Cam cam;
+static vanetza::asn1::Cpm cpm;
+static vanetza::asn1::Denm denm;
+static vanetza::asn1::Ivim ivim;
+static vanetza::asn1::Mapem mapem;
+static vanetza::asn1::Rtcmem rtcmem;
+static vanetza::asn1::Spatem spatem;
+static vanetza::asn1::Srem srem;
+static vanetza::asn1::Ssem ssem;
+static vanetza::asn1::r2::Vam vam;
+
